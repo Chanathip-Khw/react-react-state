d8aa7ef7c3a9de7858c115c8922de59cae061bf5
Oat Chana <chanathip0944729842@gmail.com>
2024-10-23 13:14:40 +0700

    feat:add greeting message when push button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMsg, setGreetingMsg] = useState("Greeting Message")
+  const thaiGreetingMsg = () => {
+    setGreetingMsg("สวัสดี!")
+  }
+  const englishGreetingMsg = () => {
+    setGreetingMsg("Hi!")
+  }
+  const chineseGreetingMsg = () => {
+    setGreetingMsg("你好!")
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMsg}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick = {thaiGreetingMsg}>สวัสดี!</button>
+        <button onClick={englishGreetingMsg}>Hi!</button>
+        <button onClick={chineseGreetingMsg}>你好!</button>
       </div>
     </div>
   );
